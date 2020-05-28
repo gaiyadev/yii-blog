ea4577f7d112745050c0bd152d81af221ae7fd50
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-05-28 22:43:33 +0100

    some customization

--- controllers/FormController.php
+++ controllers/FormController.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace app\controllers;
+use Yii;
+use app\models\TblForms;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
 
 class FormController extends \yii\web\Controller
 {
@@ -8,5 +13,18 @@ class FormController extends \yii\web\Controller
     {
         return $this->render('index');
     }
+     public function actionCreate()
+    {
+        $model = new TblNews();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
 
 }
--- views/form/index.php
+++ views/form/index.php
@@ -1,9 +1,11 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>form/index</h1>
-
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<div class="col-md-2" ></div>
+<div class="col-md-8" >
+ <?php echo'   <form action="FormController/actionCreate" method="post">'?>
+<input type="email" name="email" class="form-control" id="">
+<input type="button" value="Submit" class="btn btn-primary" >
+</form>
+</div>
+<div class="col-md-2" ></div>
